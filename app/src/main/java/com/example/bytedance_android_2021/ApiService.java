bfe2a03656e264810141e5380eb4406b1508cd17
FredNg <wjhfred@outlook.com>
2021-07-20 16:46:35 +0800

    把接口代码改成 java

---
+++ app/src/main/java/com/example/bytedance_android_2021/ApiService.java
@@ -0,0 +1,11 @@
+package com.example.bytedance_android_2021;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiService {
+    @GET("api/invoke/video/invoke/video")
+    Call<List<VideoItem>> getVideos();
+}
---
+++ app/src/main/java/com/example/bytedance_android_2021/VideoItem.java
@@ -0,0 +1,71 @@
+package com.example.bytedance_android_2021;
+
+import com.google.gson.annotations.SerializedName;
+
+public class VideoItem {
+    @SerializedName("_id")
+    private String id;
+
+    @SerializedName("feedurl")
+    private String feedUrl;
+
+    @SerializedName("nickname")
+    private String nickname;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("likecount")
+    private int likeCount;
+
+    @SerializedName("avatar")
+    private String avatar;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFeedUrl() {
+        return feedUrl;
+    }
+
+    public void setFeedUrl(String feedUrl) {
+        this.feedUrl = feedUrl;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+}
